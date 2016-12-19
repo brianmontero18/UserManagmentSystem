99be226ad3f5d72d841180399649fdf21f70e076
brianmontero18 <brianmontero18@gmail.com>
2016-12-19 01:13:43 -0300

    Second commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
@@ -15,12 +16,39 @@ gulp.task('style', function() {
 
 gulp.task('inject', function() {
     var wiredep = require('wiredep').stream;
-    var options = {
-        bowerJson: require('./bower.json'),
-        directory: './public/lib'
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css',
+                              './public/js/*.js'], {read: false});
+
+    var injectOptions = {
+        ignorePath: '/public'
     };
 
-    return gulp.src('./src/views/*html')
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./src/views/*.jade')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function() {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 5000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function(ev) {
+            console.log('Restarting...');
+        });
 });
